72aad82ba2233741126c6ca7a12b61498178c566
Ricardo <ricardo.ruizstg@gmail.com>
2023-01-31 15:54:34 -0500

    models_tweets

--- main.py
+++ main.py
@@ -1,6 +1,60 @@
+#Python
+from uuid import UUID
+from datetime import date
+from typing import Optional
+from datetime import datetime
+
+#pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+
+#FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
+
+#Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class Userlogin(UserBase):
+    password: str = Field (
+        ...,
+        min_length=8,
+        max_length= 64
+    )
+
+class User(UserBase):
+    
+    first_name: str = Field(
+        ...,
+        min_length = 1,
+        max_length = 50
+    )
+    last_name: str = Field(
+        ...,
+        min_length = 1,
+        max_length = 50
+        
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256,
+    )
+    post: datetime = Field(default=datetime.now())
+    update: Optional[datetime] = Field(default=None)
+    by: User = Field(
+        ...
+    )
 
 @app.get(
     path= "/"
